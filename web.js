5d0323f16e4715778a9eaf094d7933ee754818f6
Travis Skare <travisskare@gmail.com>
2013-07-04 11:59:02 -0700

    various typos

--- web.js
+++ web.js
@@ -1,9 +1,21 @@
+var fs = require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var index;
+fs.readFile('./index.html', function (err, data) {
+    if (err) {
+        throw err;
+    }
+    index = data;
+});
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	response.writeHeader(200, {"Content-Type": "text/html"});
+	response.write(index);
+	response.close();
 });
 
 var port = process.env.PORT || 5000;
